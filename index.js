22b60891d1f9ef7c5df744c7a1517ce67fbb5655
Yann Deshayes <deshayes.yann@gmail.com>
2017-10-10 11:19:52 +0200

    Use the platformsh helper to get environment variables

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var sys = require('sys')
 var exec = require('child_process').exec;
-var atob = require('atob');
+// Load the Platform.sh configuration.
+var config = require("platformsh").config();
 
-var base64PlatformRelationships = process.env.PLATFORM_RELATIONSHIPS;
-var base64PlatformRoutes = process.env.PLATFORM_ROUTES;
-var port = process.env.PORT;
+var port = config.port;
 
 function puts(error, stdout, stderr) {
   console.log(error);
@@ -13,14 +12,12 @@ function puts(error, stdout, stderr) {
   sys.puts(stderr);
 }
 
-if(base64PlatformRelationships && base64PlatformRoutes) {
-  var platformRelationships = JSON.parse(atob(base64PlatformRelationships));
-  var database = platformRelationships.database[0];
+var platformRelationships = config.relationships;
+var database = platformRelationships.database[0];
 
-  var platformRoutes = JSON.parse(atob(base64PlatformRoutes));
-  var rootUrl = Object.keys(platformRoutes)[0];
+var platformRoutes = config.routes;
+var rootUrl = Object.keys(platformRoutes)[0];
 
-  var cmd = "PORT="+port+" MONGO_URL="+database.scheme+"://"+database.username+":"+database.password+"@"+database.host+":"+database.port+"/"+database.path+" ROOT_URL="+rootUrl+" node ./build/bundle/main.js";
+var cmd = "PORT="+port+" MONGO_URL="+database.scheme+"://"+database.username+":"+database.password+"@"+database.host+":"+database.port+"/"+database.path+" ROOT_URL="+rootUrl+" node ./build/bundle/main.js";
 
-  exec(cmd, puts);
-}
+exec(cmd, puts);
